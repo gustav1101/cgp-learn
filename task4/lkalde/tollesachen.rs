dbbf0e4feb1bdd4133691cad58f1977a8103e9c0
Lukas Kalde <lkalde@uos.de>
2016-07-20 11:16:19 +0200

    add use statement

--- task4/lkalde/tollesachen.rs
+++ task4/lkalde/tollesachen.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 fn main() {
     let b = 4;
     let c = 3;
@@ -5,9 +7,9 @@ fn main() {
     println!("{:?}", d);
 }
 
-fn tollefunktion<T: std::ops::Mul + std::ops::Add + Copy>
+fn tollefunktion<T: ops::Mul + ops::Add + Copy>
     (v1: T,
      v2: T)
-     -> (<T as std::ops::Add>::Output, <T as std::ops::Mul>::Output) {
+     -> (<T as ops::Add>::Output, <T as ops::Mul>::Output) {
     (v1 + v2, v1 * v2)
 }
